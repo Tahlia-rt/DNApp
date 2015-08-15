573bd4b72a6d87a971d9bc4b765ddca660208a3b
Karlis Berzins <berzinsh.karlis@gmail.com>
2015-08-15 22:22:59 +0300

    Add Login controller

---
+++ DNApp/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  DNApp
+//
+//  Created by Karlis Berzins on 15/08/15.
+//  Copyright © 2015 Karlis Berzins. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var dialogView: DesignableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+// MARK: Actions
+
+    @IBAction func loginButtonDidTouch(sender: AnyObject) {
+        dialogView.animation = "shake"
+        dialogView.animate()
+    }
+}
